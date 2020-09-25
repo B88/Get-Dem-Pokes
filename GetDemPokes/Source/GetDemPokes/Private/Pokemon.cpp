7db0220d902d210b476ca8b86233a1cb9881997f
lahral <pendragonheart@gmail.com>
2020-09-25 14:57:36 -0700

    Added list of moves & started on Big List O Pokemon.
    1 mon down, 150 to finish. I hate how I've had to implement the list of moves known at a particular level, but nothing I can do aobut it right now.

--- GetDemPokes/Source/GetDemPokes/Private/Pokemon.cpp
+++ GetDemPokes/Source/GetDemPokes/Private/Pokemon.cpp
@@ -3,9 +3,29 @@
 
 #include "Pokemon.h"
 
-Pokemon::Pokemon(std::string name, int hp, int atk, int def, int spdef, int spatk, int speed, int IVhp, int IVatk, int IVdef, int IVspdef, int IVspatk, int IVspeed, int EVhp, int EVatk, int EVdef, int EVspdef, int EVstatk, int EVspeed, int baselvl, int pokedexNum)
+Pokemon::Pokemon(std::string name, int hp, int atk, int def, int spdef, int spatk, int speed, int IVhp, int IVatk, int IVdef, int IVspdef, int IVspatk, int IVspeed, int EVhp, int EVatk, int EVdef, int EVspdef, int EVspatk, int EVspeed, int baselvl, int pokedexNum)
 {
-
+	name = name;
+	hp = hp;
+	atk = atk;
+	def = def;
+	spdef = spdef;
+	spatk = spatk;
+	speed = speed;
+	IVhp = IVhp;
+	IVatk = IVatk;
+	IVdef = IVdef;
+	IVspdef = IVspdef;
+	IVspatk = IVspatk;
+	IVspeed = IVspeed;
+	EVhp = EVhp;
+	EVatk = EVatk;
+	EVdef = EVdef;
+	EVspdef = EVspdef;
+	EVspatk = EVspatk;
+	EVspeed = EVspeed;
+	baselvl = baselvl;
+	pokedexNum = pokedexNum;
 
 }
 
